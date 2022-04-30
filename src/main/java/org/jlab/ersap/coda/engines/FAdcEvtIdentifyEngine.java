2173162cc4cdf25ff7cec85eef26cbaa22b27599
gurjyan <gurjyan@jlab.org>
2022-04-30 18:29:32 -0400

    warning in case payload bank is empty

--- src/main/java/org/jlab/ersap/coda/engines/FAdcEvtIdentifyEngine.java
+++ src/main/java/org/jlab/ersap/coda/engines/FAdcEvtIdentifyEngine.java
@@ -101,7 +101,7 @@ public class FAdcEvtIdentifyEngine implements Engine {
 //            long timestamp = ((((long) intData[1]) & 0x00000000ffffffffL) +
 //                    (((long) intData[2]) << 32));
             long timestamp = frame * 65536L;
-//            System.out.println("  Frame = " + frame + ", TS = " + timestamp);
+            System.out.println("  Frame = " + frame + ", TS = " + timestamp);
 
             // Loop through all ROC Time Slice Banks (TSB) which come after TIB
             for (int j = 1; j < childCount; j++) {
