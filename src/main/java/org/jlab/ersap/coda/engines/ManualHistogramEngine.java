271c9cce006012e9018084a90fab066f5865700c
gurjyan <gurjyan@jlab.org>
2022-05-05 16:08:21 -0400

    debugging

--- src/main/java/org/jlab/ersap/coda/engines/ManualHistogramEngine.java
+++ src/main/java/org/jlab/ersap/coda/engines/ManualHistogramEngine.java
@@ -58,7 +58,7 @@ public class ManualHistogramEngine extends AbstractEventWriterService<FileWriter
             }
             System.out.println(" -------------- ");
             try {
-                Thread.sleep(2000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
