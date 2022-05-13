ece0b67c1d4126915772dec043e0cbcb67627edf
gurjyan <gurjyan@jlab.org>
2022-05-13 11:59:54 -0400

    debugging...

--- src/main/java/org/jlab/ersap/coda/engines/FAdcIdEngine.java
+++ src/main/java/org/jlab/ersap/coda/engines/FAdcIdEngine.java
@@ -85,9 +85,13 @@ public class FAdcIdEngine implements Engine {
         centerBlocks.add("1-17-6");
         centerBlocks.add("1-17-7");
         centerBlocks.add("1-17-8");
+        centerBlocks.add("1-17-9");
+        centerBlocks.add("1-17-10");
         centerBlocks.add("1-17-11");
         centerBlocks.add("1-17-12");
         centerBlocks.add("1-19-0");
+        centerBlocks.add("1-19-1");
+        centerBlocks.add("1-19-2");
         centerBlocks.add("1-19-3");
         centerBlocks.add("1-19-4");
         centerBlocks.add("1-19-5");
