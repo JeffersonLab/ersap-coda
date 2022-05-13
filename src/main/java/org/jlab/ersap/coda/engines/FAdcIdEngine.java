4c8475508a4495130bcbd140edd465b0a9a8c2c7
gurjyan <gurjyan@jlab.org>
2022-05-13 10:40:20 -0400

    debugging...

--- src/main/java/org/jlab/ersap/coda/engines/FAdcIdEngine.java
+++ src/main/java/org/jlab/ersap/coda/engines/FAdcIdEngine.java
@@ -187,6 +187,7 @@ public class FAdcIdEngine implements Engine {
                             } else if (tSlot == 0 && tChannel ==0 && bcSlot ==0 && bcChannel == 0){
                                 times.add(ht);
                                 data.add(new VAdcHit(1, slt, channel, q, ht));
+                                System.out.println("A");
 
                                 // beam center and calorimeter events
                             } else {
@@ -321,6 +322,7 @@ public class FAdcIdEngine implements Engine {
                             } else if (tSlot == 0 && tChannel == 0 &&
                                     bcSlot == 0 && bcChannel == 0) {
                                 out.setData(JavaObjectType.JOBJ, data);
+                                System.out.printf("B");
                             }
                         }
                     }
