9c0cf6ffe1e13e24c326dc10ecd873aa6871ce10
gurjyan <gurjyan@jlab.org>
2025-07-11 16:45:34 -0400

    More bugs

--- src/main/cpp/src/CodaTimeFrameBinaryDataType.cpp
+++ src/main/cpp/src/CodaTimeFrameBinaryDataType.cpp
@@ -1,17 +1,10 @@
 // CodaTimeFrameBinaryDataType.cpp
 #include "CodaTimeFrameBinaryDataType.hpp"
-#include "CodaTimeFrameDataType.hpp"
-#include <stdexcept>
-#include <cstring>
-#include <cstdint>
-#include <vector>
 
 namespace ersap {
 namespace coda {
 
-
 // Implementation moved to CodaTimeFrameDataType.cpp as static methods
-
 
 } // namespace coda
 } // namespace ersap
